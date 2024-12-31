1cc766a9778479e6f469b4bf9c6bef576f5dd8be
AXCWG <xiey0@qq.com>
2024-12-31 15:29:38 +0800

    sync

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace 文字游戏;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread 标题 = new Thread(() =>
+        {
+            Console.Clear();
+            Thread.Sleep(100);
+            Console.Write("文");
+            Thread.Sleep(100);
+            Console.Write("字");
+            Thread.Sleep(100);
+            Console.Write("游");
+            Thread.Sleep(100);
+            Console.WriteLine("戏");
+            Thread.Sleep(500);
+            Console.WriteLine("v0.1a");
+            Thread.Sleep(500);
+            
+        });
+        标题.Start();
+    }
+}
