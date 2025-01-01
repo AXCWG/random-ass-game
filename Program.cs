298aa94730f6590047b4eb80d0cd3d3ec86f2c9f
AXCWG <xiey0@qq.com>
2025-01-01 15:10:53 +0800

    drop mac support

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace 文字游戏;
+﻿using NAudio.Midi;
+using NAudio.Wave;
+
+namespace 文字游戏;
 
 class Program
 {
@@ -18,9 +21,12 @@ class Program
             Thread.Sleep(500);
             Console.WriteLine("v0.1a");
             Thread.Sleep(500);
-            #pragma warning disable CA1416
-            Console.Beep(261, 800);
-            #pragma warning restore CA1416
+            MidiOut midiOut = new MidiOut(0);
+            int channel = 1;
+            int noteNumber = 50; 
+            var noteOnEve = new NoteOnEvent(0, channel, noteNumber, 100, 50);
+            midiOut.Send(noteOnEve.GetAsShortMessage());
+            midiOut.Dispose();
         });
         标题.Start();
     }
